cc8263aaafadc99bf9f94db2357afabfbf2bf865
Mathias <mathiasgartner@gmx.at>
2020-08-17 10:30:52 +0200

    script for creating performances

--- tor/server/scripts/createPerformance.py
+++ tor/server/scripts/createPerformance.py
@@ -1,3 +1,4 @@
+import argparse
 import copy
 from datetime import datetime, timedelta
 import time
@@ -5,8 +6,19 @@ import time
 from tor.base import DBManager
 from tor.server.Job import Job
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-run", dest="run", action="store_true")
+parser.add_argument("-start", dest="start", action="store_true")
+parser.add_argument("-wait", dest="wait", action="store_true")
+parser.add_argument("-thenwait", dest="thenwait", action="store_true")
+parser.add_argument("-id", dest="id", type=int, default=None)
+args = parser.parse_args()
+
 jW = Job()
 jW.JobCode = "W"
+
+jR = Job()
+jR.JobCode = "R"
 
 j1 = Job()
 j1.JobCode = "P"
@@ -18,9 +30,26 @@ j2.JobCode = "P"
 j2.JobParameters = 2
 j2.ExecuteAt = datetime.now() + timedelta(hours=-1) + timedelta(minutes=1, seconds=20)
 
-ids = [19]
-jobsToCreate = [j2, jW]
+jobsToCreate = []
 
+if args.run:
+    jobsToCreate.append(jR)
+elif args.start:
+    jobsToCreate.append(j2)
+elif args.wait:
+    jobsToCreate.append(jW)
+
+if args.thenwait:
+    jobsToCreate.append(jW)
+
+if args.id is not None:
+    ids = int(args.id)
+
+#jobsToCreate = [j2, jW]
+#ids = [19]
+
+if not isinstance(ids, list):
+    ids = [ids]
 for jobToCreate in jobsToCreate:
     jobs = []
     for id in ids:
