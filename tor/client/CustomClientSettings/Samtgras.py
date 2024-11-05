c9591e5a497f4cabb2c69171cb7bca2f1d23d058
M on Yoga <mathiasgartner@gmx.at>
2024-11-05 21:59:34 +0100

    homing config

--- tor/client/CustomClientSettings/Samtgras.py
+++ tor/client/CustomClientSettings/Samtgras.py
@@ -1,4 +1,4 @@
 import tor.client.ClientSettings as cs
 cs.RAMP_MATERIAL = "Samtgras"
 
-cs.G_HOMING = "G28 N{} A0 S50 P130 F68 R8 D1.09 B1.15"
+cs.G_HOMING = "G28 N{} A0 S50 P130 F68 R15 D1.09 B1.14"
