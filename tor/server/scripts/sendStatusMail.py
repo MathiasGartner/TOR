b20681df59ff38ace2ae3fee2be551a4dc62b987
Mathias Gartner <mathiasgartner@gmx.at>
2024-11-10 21:56:13 +0100

    cronjob info in status mail

--- tor/server/scripts/sendStatusMail.py
+++ tor/server/scripts/sendStatusMail.py
@@ -1,4 +1,8 @@
 
+# for registering as a crontab, e.g. hourly updates with:
+# 0 * * * * cd /home/pi/TOR && sudo /home/pi/TOR/torenvRP4/bin/python3 -u -m tor.server.scripts.sendStatusMail
+# this is done on Raspi4 in <sudo crontab -e>
+
 from tor.base import DBManager
 from tor.base import MailManager
 
