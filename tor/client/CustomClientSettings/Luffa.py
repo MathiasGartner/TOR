86c9b1d0b81a7f1e99f75dc2fad21911039dd83e
Mathias Gartner <mathiasgartner@gmx.at>
2025-08-03 11:50:37 +0200

    custom client settings

--- tor/client/CustomClientSettings/Luffa.py
+++ tor/client/CustomClientSettings/Luffa.py
@@ -1,6 +1,6 @@
 import tor.client.ClientSettings as cs
 cs.RAMP_MATERIAL = "Luffa"
 
-cs.G_HOMING = "G28 N{} A0 S50 P130 F68 R8 D1.07 B1.15"
+cs.G_HOMING = "G28 N{} A0 S50 P130 F68 R8 D1.07 B1.17"
 
 cs.FAKE_BLOB_POSITIONS = [(1072, 562), (1089, 495)]
